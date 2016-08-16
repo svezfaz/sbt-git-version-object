ffddd492db8c77b23ba8516c13fc206a7bc6c663
Stefano Bonetti <svez.faz@gmail.com>
2016-08-16 18:57:19 +0100

    - adding Bintray publish task - adding Travis CI support

--- project/PluginBuild.scala
+++ project/PluginBuild.scala
@@ -1,21 +1,31 @@
 import sbt.Keys._
 import sbt._
+import bintray.Plugin._
+import bintray.Keys._
 
 object PluginBuild extends Build {
+
+  lazy val commonSettings = Seq(
+    version in ThisBuild := "0.0.1",
+    organization in ThisBuild := "eu.svez"
+  )
 
   val pluginName = "sbt-git-version-object"
 
   lazy val root = Project(pluginName, base = file("."))
+    .settings(commonSettings ++ bintrayPublishSettings: _*)
     .settings(
       sbtPlugin := true,
-      version := "0.2-SNAPSHOT",
-      organization := "eu.svez",
       scalaVersion := "2.10.4",
+      licenses += ("Apache-2.0", url("https://www.apache.org/licenses/LICENSE-2.0.html")),
+      publishMavenStyle := false,
+      repository in bintray := "sbt-plugins",
+      bintrayOrganization in bintray := None,
       libraryDependencies ++= Seq(
         "org.eclipse.jgit" % "org.eclipse.jgit"       % "4.4.1.201607150455-r",
         "org.eclipse.jgit" % "org.eclipse.jgit.junit" % "4.4.1.201607150455-r",
-        "junit"            % "junit"                  % "4.12"    % "test",
-        "org.scalatest"    %% "scalatest"             % "2.2.4"   % "test"
+        "junit"            % "junit"                  % "4.12"    % Test,
+        "org.scalatest"    %% "scalatest"             % "2.2.4"   % Test
       )
     )
 }
